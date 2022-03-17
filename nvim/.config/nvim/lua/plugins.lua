fc92f51f54ad485abdaed0cde380ee9e542f9b68
Geraint Hywel <g.s.j.hywel@gmail.com>
2022-03-17 17:11:42 +0000

    Don't load .vimrc files

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -27,7 +27,7 @@ return require("packer").startup(function()
         config = function()
             require("config-local").setup({
                 -- Default configuration (optional)
-                config_files = { ".vimrc.lua", ".vimrc" }, -- Config file patterns to load (lua supported)
+                config_files = { ".vimrc.lua" }, -- Config file patterns to load (lua supported)
                 hashfile = vim.fn.stdpath("data") .. "/config-local", -- Where the plugin keeps files data
                 autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
                 commands_create = true, -- Create commands (ConfigSource, ConfigEdit, ConfigTrust, ConfigIgnore)
