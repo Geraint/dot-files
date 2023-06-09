88f79b968c92289ce04e90a06c9396dd4ad3a0bd
Geraint Hywel <g.s.j.hywel@gmail.com>
2023-06-09 15:53:06 +0100

    Allow LSP code actions in visual mode

--- nvim/.config/nvim/lua/lsp-on-attach.lua
+++ nvim/.config/nvim/lua/lsp-on-attach.lua
@@ -19,7 +19,7 @@ function M.on_attach(client, bufnr)
   end, bufopts)
   vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 
