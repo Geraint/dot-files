0fb6345968f74fb7781c48e15b06b332aa55d54c
Geraint Hywel <g.s.j.hywel@gmail.com>
2025-09-10 10:27:14 +0100

    fix: updates for treesitter and lualine

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,23 +1,72 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
+          "bash",
           "css",
+          "csv",
           "html",
+          "git_config",
+          "git_rebase",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
           "javascript",
+          "json",
           "lua",
+          "luadoc",
+          "markdown",
+          "mermaid",
           "php",
+          "phpdoc",
+          "python",
           "ruby",
           "scss",
+          "ssh_config",
+          "xml",
+          "yaml",
           "vim",
         },
         highlight = {
           enable = true,
-          additional_vim_regex_highlighting = true,
+          additional_vim_regex_highlighting = { 'php' },
         },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahed = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              -- You can optionally set descriptions to the mappings (used in the desc parameter of
+              -- nvim_buf_set_keymap) which plugins like which-key display
+              ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+              -- You can also use captures from other query groups like `locals.scm`
+              ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            },
+            -- You can choose the select mode (default is charwise 'v')
+            --
+            -- Can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+              ['@parameter.outer'] = 'v', -- charwise
+              ['@function.outer'] = 'V', -- linewise
+              -- ['@class.outer'] = '<c-v>', -- blockwise
+              ['@class.outer'] = 'V',
+            }
+          }
+        }
       })
     end
   }
