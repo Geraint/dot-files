315bdcc2c193b9b4bdeadbf9b7678ac0776bd770
Geraint Hywel <g.s.j.hywel@gmail.com>
2026-06-01 08:16:13 +0100

    remove nvim-treesitter; add tree-sitter-manager

---
+++ nvim/.config/nvim/lua/plugins/tree-sitter-manager.lua
@@ -0,0 +1,43 @@
+return {
+  {
+      "romus204/tree-sitter-manager.nvim",
+      dependencies = {}, -- tree-sitter CLI must be installed system-wide
+      config = function()
+          require("tree-sitter-manager").setup({
+              -- Default Options
+              ensure_installed = {
+                  "bash",
+                  "css",
+                  "csv",
+                  "git_config",
+                  "git_rebase",
+                  "gitattributes",
+                  "gitcommit",
+                  "gitignore",
+                  "html",
+                  "html_tags",
+                  "javascript",
+                  "json",
+                  "lua",
+                  "luadoc",
+                  "markdown",
+                  "mermaid",
+                  "php",
+                  "phpdoc",
+                  "python",
+                  "ruby",
+                  "scss",
+                  "ssh_config",
+                  "twig",
+                  "vim",
+                  "xml",
+                  "yaml",
+              },
+              -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+              auto_install = true, -- if enabled, install missing parsers when editing a new file
+              -- highlight = true, -- treesitter highlighting is enabled by default
+              -- languages = {}, -- override or add new parser sources
+          })
+      end
+  }
+}
